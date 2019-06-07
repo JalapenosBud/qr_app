0869f4a8218e40f1d7cf3857ff37db2ab99c40ab
JalapenosBud <Jalapenosbud@gmail.com>
2019-06-07 16:49:17 +0200

    wait

--- polls/views.py
+++ polls/views.py
@@ -12,6 +12,8 @@ from django.core.files.base import ContentFile
 from django.http import HttpResponseBadRequest
 from QRCODE.settings import BASE_DIR
 
+from django.http import HttpResponse, Http404
+from django.contrib.auth.models import User
 
 def index(request):
     #context = {}
@@ -22,13 +24,12 @@ def weblink(request):
         if request.method == 'GET':
                 return render(request, "polls/weblink.html")
 
-        if request.method == 'POST':
-                model = WeblinkModel()
-                model.create(request.POST["weblink"])
-                #model.weblink = request.POST["weblink"]
-                #database save
-                model.save()
-                        #load methode kaldes her ()
+        if request.POST:
+                if '_load' in request.POST:
+                        #do_load()
+                        pass
+                elif '_generate' in request.POST:
+                        do_generate(request)
                 return render(request, 'polls/weblink.html')  
         return HttpResponseBadRequest()
 
@@ -74,3 +75,38 @@ def getOne(request, type, id):
                 context = {"qr": qr, "type": type}
                 #"getone.html" er ikke lavet men den skal udfylde 
                 return render(request, "polls/getone.html", context)
+
+def showImage(request):
+        link = "C:\\Users\\Jalap\\Desktop\\qr\\qr_app\\media\\code.png"
+        if request.method == 'GET':
+                pass
+
+
+def do_generate(request):
+        model = WeblinkModel()
+        model.create(request.POST["weblink"])# model.weblink = request.POST["weblink"]# database save
+        model.save()# load methode kaldes her()
+
+#def image_to_html(image):
+  #  return '<img src="%(path)s" title="%(title)s" alt="%(alt)s" />' % {
+     #   'path': image.raw.path,
+    #    'title': image.title,
+     #   'alt': image.alt_text
+   # }     
+
+
+#def user_page(request, username):
+     #   try:
+      # #         user = User.objects.get(username = username)
+      #  except:
+      #          raise Http404('Requested user not found.')
+##
+      ##  product = user.product_set.all()
+       # template = get_template('user_page.html')
+       # variables = Context({
+        #        'username': username,
+        #        'product': product
+     #   })
+       # output = template.render(variables)
+      #  return HttpResponse(output)
+
