ebf1ba704c52dce9171b6f86e9ea8e689eed8aa5
JalapenosBud <Jalapenosbud@gmail.com>
2019-06-10 17:43:27 +0200

    file save load input

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,9 @@ import os
 import pyqrcode
 import png
 import json
+import io
+import sys
+import base64
 
 from django.core import serializers
 from django.shortcuts import render
@@ -11,9 +14,25 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404,HttpResponseBadRequest
 from django.forms.models import model_to_dict
 from pprint import pprint
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+
 
 from django.core.files.storage import FileSystemStorage
 
+def s2b64_encode(file):
+        #bytes -> base64
+        
+        #in  filename(string)
+        #out bytes(base64)
+        return base64.b64encode(open(file, 'rb').read())
+
+def s2b64_decode(dbytes):
+        #base64 -> bytes -> string
+        
+        #in  bytes
+        #out string
+        return bytes.decode(base64.b64decode(dbytes))
 def index(request):
     #context = {}
     return render(request, 'polls/index.html')
@@ -86,14 +105,14 @@ def wifi(request):
 
                         dict_obj = model_to_dict(model)
                         #serialized = json.dumps(dict_obj)
-                        with open('data.json', 'w') as outfile:
+                        with open('C:\\Users\\Jalap\\Desktop\\qr\\qr_app\\media\\data.json', 'w') as outfile:
                                 json.dump(dict_obj, outfile)
                 
                         context = {'object': model}
                         return render(request, 'polls/wifi.html') 
 
                 if '_populate' in request.POST:
-                        with open('data.json') as data_file:
+                        with open('C:\\Users\\Jalap\\Desktop\\qr\\qr_app\\media\\data.json') as data_file:
                                 somedata = json.loads(data_file.read())
                         #wifiname = json.loads(somedata['id'][0])
 
@@ -106,10 +125,41 @@ def wifi(request):
                         return render(request, 'polls/wifi.html',context) 
 
                 if '_upload_file' in request.POST:
-                        uploaded_file = request.FILES['document']
-                        fs = FileSystemStorage()
-                        fs.save(uploaded_file.name, uploaded_file)
-                return render(request, 'polls/wifi.html')
+                        json_file = request.FILES['document']
+                        mybt = json_file.read()
+                        
+                        #path = default_storage.save('C:\\Users\\Jalap\\Desktop\\qr\\qr_app\\media\\data.json', ContentFile(json_file.read()))
+                        #asddd = s2b64_encode(path)
+
+                        #with open(json_file) as data_file:
+                        #        somedata = json.loads(data_file.read())
+                        somedata = json.loads(mybt)
+                        
+                        wifiname =     somedata['wifiName']
+                        wifiprotocol = somedata['wifiPass']
+                        wifipass =     somedata['wifiAuth']
+                        context = {'wifiname': wifiname,
+                                   'wifiauth': wifiprotocol,
+                                   'wifipass': wifipass}
+
+
+
+                        
+                        #uploaded_file = request.FILES['document']
+                        #fs = FileSystemStorage()
+                        #fs.save(uploaded_file.name, uploaded_file)
+
+                        #with open(uploaded_file) as data_file:
+                        #        somedata = json.loads(data_file.read())
+                        #wifiname = json.loads(somedata['id'][0])
+
+                        #wifiname =     somedata['wifiName']
+                        #wifiprotocol = somedata['wifiPass']
+                        #wifipass =     somedata['wifiAuth']
+                        #context = {'wifiname': wifiname,
+                        #           'wifiauth': wifiprotocol,
+                        #           'wifipass': wifipass}
+                return render(request, 'polls/wifi.html', context)
         return HttpResponseBadRequest() 
 
 
